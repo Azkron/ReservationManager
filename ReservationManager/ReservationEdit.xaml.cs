d054b2e9c4f656c4afbb8e6f44e00b2c83109a33
AZKRON\Hugo <hugobeny@gmail.com>
2017-05-24 22:32:45 +0200

    Started ReservationEdit

--- ReservationManager/ReservationEdit.xaml.cs
+++ ReservationManager/ReservationEdit.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using PRBD_Framework;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,13 +21,75 @@ using System.Windows.Shapes;
 namespace ReservationManager
 {
     /// <summary>
-    /// Interaction logic for ReservationEdit.xaml
+    /// Interaction logic for Reservation.xaml
     /// </summary>
-    public partial class ReservationEdit : UserControl
+    public partial class ReservationEdit : UserControlBase
     {
-        public ReservationEdit()
+        public int? reservation = null;// save the id of the show in case the model gets reloaded from other tab
+        private Reservation Reservation = null;
+        //private ReservationsView reservationsView;
+
+        public ICommand DeleteCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
+
+        private ClientsView Clients = null;
+
+        public ReservationEdit(Reservation reservation, bool isNew = false)
         {
+            Reservation = reservation;
             InitializeComponent();
+
+            DataContext = this;
+
+            ReadOnly = App.Rights(Table.SHOW) != Right.ALL;
+            IsNew = isNew;
+
+        }
+
+
+        private bool isNew;
+
+        public bool IsNew
+        {
+            get { return isNew; }
+            set
+            {
+                isNew = value;
+                RaisePropertyChanged(nameof(IsNew));
+
+                if (isNew)
+                {
+                    btnDelete.Visibility = Visibility.Collapsed;
+                    btnCancel.Visibility = Visibility.Collapsed;
+                }
+                else if (!ReadOnly)
+                {
+                    btnDelete.Visibility = Visibility.Visible;
+                    btnCancel.Visibility = Visibility.Visible;
+                }
+
+            }
+        }
+
+
+        private bool readOnly;
+        public bool ReadOnly
+        {
+            get { return readOnly; }
+
+            set
+            {
+                readOnly = value;
+                if (readOnly)
+                {
+                    btnDelete.Visibility = Visibility.Collapsed;
+                    btnSave.Visibility = Visibility.Collapsed;
+                    btnCancel.Visibility = Visibility.Collapsed;
+                }
+
+                RaisePropertyChanged(nameof(ReadOnly));
+            }
         }
     }
 }
